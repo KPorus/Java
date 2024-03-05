b06248c94fcc99166963cff317f6ddb8608dacd3
KPorus <designerfardin2021@gmail.com>
2024-03-05 19:07:07 +0600

    modified code

--- AbstractClass.java
+++ AbstractClass.java
@@ -1,14 +1,16 @@
 abstract class test{
     protected int a;
     abstract void greet(int a); // abstruct mehtod
-    void greet2(){}; // not abstract mehtod
+    void greet2(){
+        System.out.println("From abstruct non abstruct");
+    }; // not abstract mehtod
 }
 
 class test2 extends test{
     void greet(int a)
     {
         this.a= a;
-        System.out.println("Hello"+this.a);
+        System.out.println("Hello"+""+this.a);
     }
 }
 
@@ -18,5 +20,6 @@ public class AbstractClass {
         // dynamic mehtod dispatch
         test obj = new test2();
         obj.greet(10);
+        obj.greet2();
     }
 }
