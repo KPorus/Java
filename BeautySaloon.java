3a06d2483aa6f8c3d47db064a52aafa0c46d7aed
KPorus <designerfardin2021@gmail.com>
2024-03-30 10:14:53 +0600

    modified

--- BeautySaloon.java
+++ BeautySaloon.java
@@ -6,13 +6,6 @@ class Customer {
     private String memberType;
     private Visit visit;
 
-    public Customer(String name, Visit visit) {
-        this.name = name;
-        this.visit = visit;
-        this.member = false;
-        this.memberType = "None";
-    }
-
     public String getName() {
         return name;
     }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,7 @@
-void name();
+
+    // public Customer(String name, Visit visit) {
+    //     this.name = name;
+    //     this.visit = visit;
+    //     this.member = false;
+    //     this.memberType = "None";
+    // }
